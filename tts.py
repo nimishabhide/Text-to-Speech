4b7c5787f5edc3d1598fa63f25e313ebd7bb8f4c
nimishabhide <55435515+nimishabhide@users.noreply.github.com>
2020-10-15 20:00:50 +0530

    Create tts.py

---
+++ tts.py
@@ -0,0 +1,27 @@
+import streamlit as st
+from gtts import gTTS
+import os
+st.set_option('deprecation.showfileUploaderEncoding', False)
+
+html_temp = """
+    <div style="background-color:black ;padding:10px">
+    <h1 style="color:white;text-align:center;">REGRESSION AND CLASSIFICATION ON THE GO!!</h1>
+    </div>
+    """
+st.markdown(html_temp, unsafe_allow_html=True)
+html_temp69 = """
+    <div style="background-color:white ;padding:10px">
+    <h3 style="color:black;text-align:center;">PLEASE HAVE A LOOK AT THE SIDEBAR TO MAKE THE BEST USE OF this WEBAPP</h3>
+    </div>
+    """
+st.markdown(html_temp69, unsafe_allow_html=True)
+st.sidebar.header("RegClass")
+st.sidebar.markdown('<b>Just upload a .txt file and it will read it out to you</b>', unsafe_allow_html=True)
+st.sidebar.markdown('<b>This helps you in meeting your fast approaching deadlines by being highly efficient.</b>', unsafe_allow_html=True)
+st.sidebar.markdown('<b>Created by:Nimisha Bhide</b>', unsafe_allow_html=True)
+st.sidebar.markdown('<b>Email id:nbhide.nb@gmail.com</b>', unsafe_allow_html=True)
+t = st.text_input('Input your sentence here:') 
+language="en"
+output=gTTS(text=t,lang=language,slow=False)
+output.save('output.mp3')
+os.system("start output.mp3")
