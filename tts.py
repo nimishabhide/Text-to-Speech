36cbccada66ee455d4b09352fd79836d2e467dfb
nimishabhide <55435515+nimishabhide@users.noreply.github.com>
2020-10-16 10:06:32 +0530

    Update tts.py

--- tts.py
+++ tts.py
@@ -15,14 +15,18 @@ html_temp69 = """
     </div>
     """
 st.markdown(html_temp69, unsafe_allow_html=True)
-st.sidebar.header("RegClass")
+st.sidebar.header("Your friend who will take the place of your grandma and read out stories to you")
 st.sidebar.markdown('<b>Just copy-paste your text here and I will read it out to you</b>', unsafe_allow_html=True)
 st.sidebar.markdown('<b>This helps you in meeting your fast approaching deadlines by being highly efficient.</b>', unsafe_allow_html=True)
 st.sidebar.markdown('<b>Created by:Nimisha Bhide</b>', unsafe_allow_html=True)
 st.sidebar.markdown('<b>Email id:nbhide.nb@gmail.com</b>', unsafe_allow_html=True)
-t = st.text_input('Input your sentence here:') 
+uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf")
+if uploaded_file is not None:
+    df = extract_data(uploaded_file)
+    fh=open(df,"r")
+    myText=fh.read()
 language="en"
-output=gTTS(text=t,lang=language,slow=False)
+output=gTTS(text=myText,lang=language,slow=False)
 output.save("voice.ogg")
 audio_file = open('voice.ogg', 'rb')
 audio_bytes = audio_file.read()
